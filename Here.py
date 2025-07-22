bb2afb1836b12e9693fccca8f8b7d8225efb4094
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-07-22 17:14:22 +0300

    Add files via upload

---
+++ Here.py
@@ -0,0 +1,2 @@
+print('The pace is superb and amazing')
+print("He is older than me by + 14 + Years")
---
+++ control_flow.py
@@ -0,0 +1,27 @@
+age = 23
+
+if age >=19:
+ print("Make it asignificant")
+
+else:
+  print("Nothing better")
+
+score = int(input("Enter the student's score:"))
+
+if score >= 92:
+  grade = 'A'
+
+elif score >= 80:
+  grade = 'B'
+
+elif score >= 65:
+  grade = 'C'
+
+else:
+  grade = 'Repeat'
+
+print(f('The Student grade is: {grade}'))
+
+
+
+
